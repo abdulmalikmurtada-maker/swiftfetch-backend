6692ae969634b85a74f30f54524f20c8debfd2cb
abdulmalikmurtada-maker <abdulmalikmurtada@gmail.com>
2026-06-28 11:22:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -163,12 +163,13 @@ app.get("/download", async (req, res) => {
       noWarnings: true,
     };
 if (formatId && formatId !== "mp3") {
-    options.format = `${formatId}+bestaudio/best`;
-} else if (formatId === "mp3" || fileExt === "mp3") {options.extractAudio = true;
     options.format = formatId + "+ba/" + formatId;
-    } else {
-      options.format = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best";
-    }
+} else if (formatId === "mp3" || fileExt === "mp3") {
+    options.extractAudio = true;
+    options.audioFormat = "mp3";
+} else {
+    options.format = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best";
+}
     const download = ytdlp.exec(url, options, { stdio: ['ignore', 'pipe', 'ignore'] });
     download.stdout.pipe(res);
 
